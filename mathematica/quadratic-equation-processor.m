7d86ac1e87d23f6f2fdbde172a887647d98b07fe
Matthew Connelly <mattconndev@gmail.com>
2017-05-14 02:31:21 -0400

    Removal of vertex, line plots from graph; addition of standard form fn; minor syntax clean-up

--- mathematica/quadratic-equation-processor.m
+++ mathematica/quadratic-equation-processor.m
@@ -1,34 +1,28 @@
 (* ::Package:: *)
 
-(* ::Code:: *)
+f[a_,b_,c_]:=a*x^2+b*x+c (*quadratic standard form*)
 Manipulate[
 	{
-		(*Plot[a*x^2+b*x+c,{x,-10,10},(*plot standard form*)*)
-		Plot[{a*x^2+b*x+c,a2*x^2+b2*x+c2,firstRoot,secondRoot,vertexXCoord,vertexYCoord,yIntercept},{x,-10,10},(*plot standard form*)
-		PlotLegends-> {
-		StringForm["``x^2 ``x ``",
-					a,
-					NumberForm[b,NumberSigns->{"-", "+"}],
-					NumberForm[c,NumberSigns->{"-", "+"}]
-				],
-	StringForm["``x^2 ``x ``",
-					a2,
-					NumberForm[b2,NumberSigns->{"-", "+"}],
-					NumberForm[c2,NumberSigns->{"-", "+"}]
-			],
-			"First Root",
-			"Second Root",
-			"Vertex x-coord.",
-			"Vertex y-coord.",
-			"y-intercept"
-		}
-			(* PlotLabel->Style[ (*display standard form above plot*)
+		Plot[{f[a,b,c],f[a2,b2,c2]},
+			{x,-10,10},
+			ImageSize->Large,
+			PlotLegends-> {
 				StringForm["``x^2 ``x ``",
 					a,
 					NumberForm[b,NumberSigns->{"-", "+"}],
 					NumberForm[c,NumberSigns->{"-", "+"}]
 				],
-			FontSize->24] *)
+				StringForm["``x^2 ``x ``",
+					a2,
+					NumberForm[b2,NumberSigns->{"-", "+"}],
+					NumberForm[c2,NumberSigns->{"-", "+"}]
+				],
+				"First Root",
+				"Second Root",
+				"Vertex x-coord.",
+				"Vertex y-coord.",
+				"y-intercept"
+			}
 		],(*end plot*)
 		{ (*display roots, vertex, y-intercept*)
 			StringForm["\n 1:\n roots: (``,0), (``,0) \n vertex: (``,``) \n y-intercept: (0,``) \n \n 
@@ -45,18 +39,23 @@ Manipulate[
 				vertexYCoord=-a2*vertexXCoord^2+b2*vertexXCoord+c2,
 				yIntercept=c2
 			]
-	}(*end display group*)
+		}(*end display group*)
 	},(*end manipulated group*)
 
 	(*manipulation sliders*)
-	{a,-20,20},{b,-20,20},{c,-20,20},{a2,-20,20},{b2,-20,20},{c2,-20,20},
+	{a,-20,20},{b,-20,20},{c,-20,20},(*parabola 1*)
+	{a2,-20,20},{b2,-20,20},{c2,-20,20},(*parabola 2*)
+
 	(*toggle terms*)
+	(*parabola 1*)
 	Control[{{a,0,"1: ax^2"},{1,0}}],
 	Control[{{b,0,"bx"},{1,0}}],
 	Control[{{c,0,"c"},{1,0}}],
-Control[{{a2,0,"2: ax^2"},{1,0}}],
-Control[{{b2,0,"bx"},{1,0}}],
-Control[{{c2,0,"c"},{1,0}}]
+
+	(*parabola 2*)
+	Control[{{a2,0,"2: ax^2"},{1,0}}],
+	Control[{{b2,0,"bx"},{1,0}}],
+	Control[{{c2,0,"c"},{1,0}}]
 ]
 
 

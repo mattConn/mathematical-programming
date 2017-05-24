4b3be60358e0a442c4b9e3e83ff50d8d41de954e
Matthew Connelly <mattconndev@gmail.com>
2017-05-24 04:02:06 -0400

    Addition of minor comments

--- vertex-parabolic-trajectory.m
+++ vertex-parabolic-trajectory.m
@@ -1,18 +1,26 @@
 (* ::Package:: *)
 
+(*standard quadratic form, no y-intercept*)
 f[x_,b_]:=x^2+b*x
 
+(*list of all plotted parabolas, points etc*)
 plots={}
 
 For[b=-20,b<=20,b++,
 	AppendTo[plots,{
-		Plot[{f[x,b]},{x,-16,16}],
+		Plot[x^2+b*x,{x,-16,16}],
 		Graphics[{
 			PointSize[Medium],
 			Red,
-			Point[{-b/2,(-b/2)^2+b*(-b/2)}]
+			Point[{-b/2,(-b/2)^2+b*(-b/2)}] (*plot each parabola's vertex*)
 		}]
 	}](*end AppendTo*)
 ]
 
 Show[plots]
+
+
+
+
+
+

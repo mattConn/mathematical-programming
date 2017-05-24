ef49af1a047e273f40a0d2c7454c9d46620b599a
Matthew Connelly <mattconndev@gmail.com>
2017-05-24 16:00:53 -0400

    Addition of color coding; pending best color scheme

--- vertex-parabolic-trajectory.m
+++ vertex-parabolic-trajectory.m
@@ -8,16 +8,19 @@ plots={}
 
 For[b=-20,b<=20,b++,
 	AppendTo[plots,{
-		Plot[x^2+b*x,{x,-16,16}],
+		Plot[x^2+b*x,{x,-16,16},PlotStyle->RGBColor[Mod[b,2],0.3,Mod[b,2]*0.3]],
 		Graphics[{
-			PointSize[Medium],
-			Red,
+			PointSize[Large],
+			RGBColor[Mod[b,2],0.3,Mod[b,2]*0.3],
 			Point[{-b/2,(-b/2)^2+b*(-b/2)}] (*plot each parabola's vertex*)
 		}]
 	}](*end AppendTo*)
 ]
 
 Show[plots]
+
+
+
 
 
 

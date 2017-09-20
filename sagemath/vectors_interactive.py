1550b385f3b34702493d3d0a4d9dff24573a61ce
Matthew Connelly <mattconndev@gmail.com>
2017-09-20 18:54:43 -0400

    plot labels now display actual x and y

--- sagemath/vectors_interactive.py
+++ sagemath/vectors_interactive.py
@@ -9,7 +9,7 @@ def r(d):
 @interact
 def vectors(x1=r(1),y1=r(0),x2=r(0),y2=r(1)):
     show(
-    plot(arrow( (0,0), (x1,y1), legend_label="(x1,y1)", legend_color="blue"))+ #vector 1
-    plot(arrow( (x1,y1), (x1+x2, y1+y2), legend_label="(x2,y2)", legend_color="red", color="red" ))+ #vector 2, translated
-    plot(arrow( (0,0), (x1+x2, y1+y2), color="purple", legend_label="v1+v2", legend_color="purple" )) #resultant of vector 1 and 2
+    plot(arrow( (0,0), (x1,y1), legend_label=(x1,y1), legend_color="blue"))+ #vector 1
+    plot(arrow( (x1,y1), (x1+x2, y1+y2), legend_label=(x2,y2), legend_color="red", color="red" ))+ #vector 2, translated
+    plot(arrow( (0,0), (x1+x2, y1+y2), color="purple", legend_label=(x1+x2,y1+y2), legend_color="purple" )) #resultant of vector 1 and 2
     , figsize=4)

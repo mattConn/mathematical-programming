93f2696c83ee85be93c0a8bc66b546a02fcb37a5
Matthew Connelly <mattconndev@gmail.com>
2017-12-29 02:20:26 -0500

    addition of sum consecutive ints., separation of leibniz series scripts

---
+++ approximating_pi/pi-gregory-leibniz-series.py
@@ -0,0 +1,19 @@
+# pi = 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - ...)
+
+MAX = 10**6
+
+pi = 4.0
+divisor = 1
+sign = 1
+
+for i in range(1,MAX+1):
+	sign *= -1
+	divisor += 2
+	pi += sign*(4.0/divisor)
+	
+	if(sign < 0):
+		signStr = "-"
+	else:
+		signStr = "+"
+
+	print "4 " + signStr + " (4/" + str(divisor)  + ") = " + str(pi)

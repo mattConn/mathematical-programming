6794e90de04e6e285ea2e1ca924201eabfa0a070
Matthew Connelly <mattconndev@gmail.com>
2018-03-26 01:20:38 -0400

    Revision of gl-series program for better interaction; recorded output of 1e6 iterations

--- approximating_pi/gregory-leibniz-series-c/gl-series.c
+++ approximating_pi/gregory-leibniz-series-c/gl-series.c
@@ -11,24 +11,26 @@ pi = - - - + - - - + -  . . .
 #include <stdio.h>
 #include <stdlib.h>
 
-// 20 digits of pi (rounded up)
-#define PI 3.1415926535897932385
-
-#define MAX_ITER 1.0E9
+//#define MAX 1.0E9
 
 int main(int argc, char *argv[])
 {
-	double my_pi = argc < 2 ? 0 : atof(argv[1]); // initial value of pi; default to 0 if no argument in argv
-		
-	int sign = -1;
+	const char PI[] = "3.1415926535897932385";
+	double my_pi = 4.0; // initial value of pi
+	int MAX = argc < 2 ? 1E6 : atoi(argv[1]); // max iterations; default to 1E6 if no argv
+
+	int sign = 1;
 	int divisor = 1;
-	for(int i = 0; i <= MAX_ITER; i++)
+	int counter = 0;
+	while(counter < MAX)
 	{
-		my_pi += (sign *= -1) * (4.0/divisor);
 		divisor += 2;
+		my_pi += (sign *= -1) * (4.0/divisor);
+		counter++;
+		//printf("%.19f\n",my_pi);
 	}
 
-	printf("%.19f Calculated\n%.19f Actual\n", my_pi, PI) ;
+	printf("\n%d Iterations\n%d/%d Last addition\n%.19f Calculated\n%s Actual\n\n", counter, (sign*4), divisor, my_pi, PI) ;
 
 	return 0;
 }

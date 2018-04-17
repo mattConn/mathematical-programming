c334b8f88538dbd3c1e73d6dae33465826a3d0c9
Matthew Connelly <mattconndev@gmail.com>
2018-04-17 17:55:42 -0400

    addition of ln2 BigInt constant

--- nat-log2-series/nat-log2-series-binary/main.jl
+++ nat-log2-series/nat-log2-series-binary/main.jl
@@ -1,25 +1,25 @@
 include("functions.jl")
 
+log2 = 0.69314718055994506418215905796387232840061187744140625
 
-k = 2
-d = k^2
+k = 1
+d = 15
 
 s1 = getPartSum(modQuotient, k, d)
-# s2 = getPartSum(quotient, d+1, d+11) 
-s2 = 0
+
+s2 = getPartSum(quotient, d+1, d+4)
 
 s3 = s1 + s2
 
-println(s1)
-println(s2)
-println(s3)
-
 println(log(2))
-println("ln2 and approximation in bin")
+println(s1, " + ", s2, " = ", s3)
 
-println( m2bin(1*log(2)) )
+
+println(d)
+println(m2bin(log2), " : ln(2)")
 for i in 1:d
 	print(" ")
 end
 print("^\n")
-println( m2bin(s3) )
+
+println(m2bin(s3), " : approximation")

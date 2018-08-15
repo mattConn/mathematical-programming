56067a7e25f20c35336763c576585fc0bdeda28c
Matthew Connelly <matthew.b.connelly@gmail.com>
2018-08-14 20:20:33 -0400

    sum consecutive squares program now used unsigned long long for sum and iterator, sums up to 10^6 squares

--- sum_consecutive_squares.c
+++ sum_consecutive_squares.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-	double s = 0; // sum
+	unsigned long long s = 0; // sum
 
 	int j = 1;
-	for(double i = 1; i <= 1e5; i++)
+	for(unsigned long long i = 1; i <= 1e6; i++)
 	{
 		s += i * i;
 
 		if(log10(i) == j)
 		{
-			printf("10^%d : %f\n",j,s);
+			printf("10^%d : %llu\n",j,s);
 			j++;
 		}
 	}
